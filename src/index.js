ea800a7a9f480b34c5baf2b846b5bcf211e1b60f
cj <2012cjz@gmail.com>
2019-09-25 18:23:17 +0900

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import * as theFn from './function.js';
+import $ from "jquery";
+window.$ = $;
+window.jQuery = $;
+export default $;
+// import './test.scss';
+
+
+
+// if(!theFn.isTouchDevice()){}
+
+
